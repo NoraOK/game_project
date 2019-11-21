484a4d1b5cd702d83eb1597c5151d4c2fd2b49b8
NoraOK <n.okeeffe35@gmail.com>
2019-11-20 16:56:24 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/static'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.urlencoded({ extended: true }));
+const server = app.listen(8001, () => console.log('listening on port 8000 <3'));
+const io = require('socket.io')(server);
+
+var chat = {}
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.get('/game_1', (req, res) => {
+    res.render('game_1')
+});
+
+io.on('connection', function (socket) {
+    console.log('socket connected <3 <3 <3')
+    socket.on('sending_msg', function (data) {
+        console.log(data);
+        chat += data;
+        io.emit("all_msgs", data);
+    })
+
+
+})
+
