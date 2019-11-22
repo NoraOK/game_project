dfee5ef6eb04a47ebc075ffb3cef7c5917fca470
NoraOK <n.okeeffe35@gmail.com>
2019-11-21 21:42:51 -0800

    added rps game

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/static/css'));
+app.use(express.static(__dirname + '/static'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.urlencoded({ extended: true }));
@@ -29,7 +29,9 @@ var world = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
+var rps_game = [];
 
+var push_count = 0;
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -38,6 +40,56 @@ app.get('/', (req, res) => {
 app.get('/game_1', (req, res) => {
     res.render('game_1')
 });
+
+app.get('/game_2',(req, res) =>{
+    res.render('game_2')
+});
+
+
+function resolve_rps(){
+    if(rps_game.length == 2){
+        //player 1: rock, player 2: paper wins
+        if(rps_game[0].val == 'rock' && rps_game[1].val == 'paper'){
+            io.emit('player_won',{winner: rps_game[1].player, winner_count : rps_game[1].count})
+        }
+        //player 1: rock wins, player 2: scissors
+        if(rps_game[0].val == 'rock' && rps_game[1].val == 'scissors'){
+            io.emit('player_won',{winner: rps_game[0].player})
+        }
+        //player 1: rock TIE, player 2: rock TIE
+        if(rps_game[0].val == 'rock' && rps_game[1].val == 'rock'){
+            io.emit('tie_game',"Tie game, PLAY AGAIN!")
+        }
+        //player 1: paper wins, player 2: rock 
+        if(rps_game[0].val == 'paper' && rps_game[1].val == 'rock'){
+            io.emit('player_won',{winner: rps_game[0].player})
+        }
+        //player 1: paper , player 2: scissors wins 
+        if(rps_game[0].val == 'paper' && rps_game[1].val == 'scissors'){
+            io.emit('player_won',{winner: rps_game[1].player})
+        }
+        //player 1: paper TIE, player 2: paper TIE 
+        if(rps_game[0].val == 'paper' && rps_game[1].val == 'paper'){
+            io.emit('tie_game',"Tie game, PLAY AGAIN!")
+        }
+        //player 1: scissors , player 2: rock wins 
+        if(rps_game[0].val == 'scissors' && rps_game[1].val == 'rock'){
+            io.emit('player_won',{winner: rps_game[1].player, winner_count : rps_game[1].count, loser: rps_game[1].player})
+        }
+        //player 1: scissors wins, player 2: paper  
+        if(rps_game[0].val == 'scissors' && rps_game[1].val == 'paper'){
+            io.emit('player_won',{winner: rps_game[0].player})
+        }
+        //player 1: scissors TIE, player 2: scissors TIE  
+        if(rps_game[0].val == 'scissors' && rps_game[1].val == 'scissors'){
+            io.emit('tie_game',"Tie game, PLAY AGAIN!")
+        }
+        console.log(rps_game)
+        rps_game = []
+        console.log(rps_game)
+    }
+
+}
 
 io.on('connection', function (socket) {
     console.log('socket connected <3 <3 <3')
@@ -50,7 +102,20 @@ io.on('connection', function (socket) {
         chat += data;
         io.emit("all_msgs", data);
     })
+    socket.on('push_rock', function(data){
+        rps_game.push(data);
+        resolve_rps()
 
+    })
+    socket.on('push_paper', function(data){
+        rps_game.push(data); 
+        resolve_rps()
+
+    })
+    socket.on('push_scissors', function(data){
+        rps_game.push(data);
+        resolve_rps()
+    })
 
 })
 
